66dbd76ab96c8252008ff374b4dd465168bee112
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-25 10:03:26 +1100

    removed links.rb file, added tagging functionality for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+
+  # SP 25/02/2014 acts-as-taggable relationship (alias for acts_as_taggable_on :tags)
+  # gives access to information inside @links.tags
+  acts_as_taggable
 end
