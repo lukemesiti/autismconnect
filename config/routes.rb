934d99eaefb9a31a6f5e4e9994e604702dab5009
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-24 16:11:39 +1100

    added pages routes for host to access

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Autismconnect::Application.routes.draw do
-  resources :events
-
   devise_for :users
+
+  # SP 24/2/2homepage goes to views > pages > home.html.erb
   root :to => "pages#home"
 
+  # SP 24/2/2014 whenever you go /<something> this will point you to pages > views > <something>.html.erb
   get ':action' => 'pages#action'
 
 end
