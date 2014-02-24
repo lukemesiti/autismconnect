ba2d1316604e62e29579be1c45c74664801fc6eb
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-24 23:14:29 +1100

    added acts_as_taggable relationship to the article model

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,3 +1,8 @@
 class Articles < ActiveRecord::Base
   belongs_to :user
+
+  # SP 24/02/2014 acts-as-taggable relationship (alias for acts_as_taggable_on :tags)
+  # gives access to information inside @articles.tags
+  acts_as_taggable
+
 end
