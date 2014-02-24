7771e115b7a2b1b49e48daee0cdab9603c3076ba
lukemesiti <lukemesiti@gmail.com>
2014-02-24 12:29:48 +1100

    basic rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autismconnect::Application.config.secret_key_base = '2c2831fbf8fd99c12c8322bb22f2e76c43cd181817c82f5a12a0b5d7ed54aa0e0eafd99dcf64db9439b548e98c740f38f4b7fa3e7a4f7f97812b4b91f388e814'
