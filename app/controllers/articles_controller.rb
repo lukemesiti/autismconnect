d6f402bf849482092da01c048cc105796050c6b7
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-25 09:53:39 +1100

    added tagging functionality for events

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,6 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
         format.json { render action: 'show', status: :created, location: @article }
       else
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   has_many :invites
+
+  # SP 25/02/2014 acts-as-taggable relationship (alias for acts_as_taggable_on :tags)
+  # gives access to information inside @events.tags
+  acts_as_taggable
 end
