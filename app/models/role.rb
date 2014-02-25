1a313204000d44c5a61f1bc9f263fd0b981b8ec3
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-25 15:44:06 +1100

    Made changes to users_controller, users.rb and roles.rb --> When editing a profile, only an admin can set or remove the admin role --> Users who are not an admin aren't able to set themselves as admins --> On sign-up, users can only elect to be a parent or a professional --> if a user does not select a role, the role 'parent' is set as default

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,16 @@
 class Role < ActiveRecord::Base
-    has_many :users
+  has_many :users
+
+  def admin?
+    self.name == "admin"
+  end
+
+  #this determines the default role everybody is given on sign_up
+  def self.default_role
+    self.find_by(name: "parent")
+  end
+
+  def self.admin_role
+  	self.find_by(name: "admin")
+  end
 end
