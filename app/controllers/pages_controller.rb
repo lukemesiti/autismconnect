c596c07302bab7b7507feff30b22830aa35381a5
Lauren Krueger <lukru489@gmail.com>
2014-02-27 09:51:11 +1100

    Worked on the information page. Added carousel for articles as they are added to the database. Sorted by recently created_at first. Added the logo to the top navigation. Links to home. Added 'pages' stylesheet for standalone pages (about, information, home) Uploaded a document for testing to the documents section.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,13 +5,20 @@ class PagesController < ApplicationController
 	def home
     @events = Event.all.order('created_at DESC').limit(3)
 
-    @articles = Article.all
-    @article = Article.first
+    @articles = Article.all.order('created_at DESC').limit(3)
 	end
 
 	def about
 	end
 
+  def information
+    @articles = Article.all
+    @links = Link.all
+    @documents = Document.all
+
+    # to display 3 articles in carousel at a time
+    @articles_sliced = @articles.each_slice(3)
+  end
 
 
 end
