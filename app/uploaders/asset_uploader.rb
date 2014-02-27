617d8f19376e752767c0b9aed628388dca7e98f7
lukemesiti <lukemesiti@gmail.com>
2014-02-28 10:57:32 +1100

    default image added for events

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -19,13 +19,14 @@ class AssetUploader < CarrierWave::Uploader::Base
   def store_dir
     "app/assets/uploads"
   end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("app/assets/uploads/" + [version_name, "default.png"].compact.join('_'))
+  
+    # "assets/uploads/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
