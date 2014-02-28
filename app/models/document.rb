4ca9075af003acae2f9a00e4890748c9dde14b84
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-28 12:06:46 +1100

    updated shows for links and documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,10 @@ class Document < ActiveRecord::Base
   belongs_to :user
   mount_uploader :asset, AssetUploader
 
+  # acts-as-taggable relationship (alias for acts_as_taggable_on :tags)
+  # gives access to information inside @articles.tags
+  acts_as_taggable
+
   def to_s
   	self.name
   end
